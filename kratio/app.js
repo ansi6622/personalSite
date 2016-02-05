137e23eff4d425c2f04e43343fe72735a12e7b44
Anthony Simpson <ansi6622@colorado.edu>
2016-02-05 10:45:26 -0700

    who knows, its better than it was but i gotta actually do the jquery still

--- kratio/app.js
+++ kratio/app.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  $('#button p').on('mouseenter', function() {
+  $('td').on('mouseenter', function() {
     $(this).addClass('highlight');
     $(this).find('.bold').animate({'opacity': '1'});
   });
-  $('#button').on('mouseleave', function() {
+  $('aside').on('mouseleave', function() {
     $(this).removeClass('highlight');
   });
   $('#tab').on('mouseenter', function() {
     $(this).addClass('highlight');
     $(this).find('#tab').animate({'opacity': '1'});
   });
-  $("#toggle").click(function(){
-    $(this).toggle("explode")
+  $("aside").click(function(){
+    $(this).aside("explode")
   })
 });
